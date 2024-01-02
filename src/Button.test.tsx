66a4d34ab8ccddf8a1a9e0a56a7cef0f5cd00f64
Jeff Wen <sinchangwen@gmail.com>
2024-01-02 02:16:59 +0000

    chore: fix lint

--- src/Button.test.tsx
+++ src/Button.test.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react'
 import { Button } from './Button'
 
-describe('Button', async () => {
+describe('button', async () => {
   it('should render the button', () => {
     const { getByText } = render(
       <Button text="Click me" />,
